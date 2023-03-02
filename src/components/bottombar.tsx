870c91eec1707329450f174bc912bba9a05c5413
rama <akbar.6b@gmail.com>
2023-03-02 23:51:33 +0700

    add margin

--- src/components/bottombar.tsx
+++ src/components/bottombar.tsx
@@ -2,7 +2,7 @@ import ButtonTheme from "./buttonTheme";
 
 export default function Bottombar() {
   return (
-    <footer className="border-t border-neutral-100 px-5 py-7 text-center text-sm text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
+    <footer className="mt-6 border-t border-neutral-100 px-5 py-7 text-center text-sm text-neutral-500 dark:border-neutral-800 dark:text-neutral-400">
       <div className="mx-auto flex w-full max-w-3xl justify-between ">
         <div>©2022. Made by Ramaakbar</div>
         <ButtonTheme />
