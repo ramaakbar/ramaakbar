7aeaa754fbf8adfbffa2794a5366c9c6d4f7d313
rama <akbar.6b@gmail.com>
2023-03-02 23:32:49 +0700

    fix meta theme-color

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -7,19 +7,10 @@ type Props = {
 };
 export default function Layout({ children }: Props) {
   return (
-    <>
-      <Head>
-        <meta
-          name="theme-color"
-          media="(prefers-color-scheme: dark)"
-          content="#171717"
-        />
-      </Head>
-      <div className="flex min-h-screen flex-col">
-        {/* <Navbar /> */}
-        <main className="mt-6 flex-1 px-5 md:mt-12">{children}</main>
-        <Bottombar />
-      </div>
-    </>
+    <div className="flex min-h-screen flex-col">
+      {/* <Navbar /> */}
+      <main className="mt-6 flex-1 px-5 md:mt-12">{children}</main>
+      <Bottombar />
+    </div>
   );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,18 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: light)"
+          content="white"
+        />
+        <meta
+          name="theme-color"
+          media="(prefers-color-scheme: dark)"
+          content="#171717"
+        />
+      </Head>
       <body className="bg-white font-sans text-neutral-900 antialiased dark:bg-neutral-900 dark:text-neutral-50">
         <Main />
         <NextScript />
