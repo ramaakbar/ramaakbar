ec32ba28e7809e332a64a21eaffc9434761c4ff8
rama <akbar.6b@gmail.com>
2023-03-28 14:40:35 +0700

    add analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        <script
+          defer
+          data-domain="ramaakbar.xyz"
+          src="https://analytics.ramaakbar.xyz/js/script.js"
+        ></script>
+      </Head>
       <body className="bg-white font-sans text-neutral-900 antialiased dark:bg-neutral-900 dark:text-neutral-50">
         <Main />
         <NextScript />
